02ecec9f3391001fd9f920c41ad2f2bf3dacd8e1
soulriaper <soulriaper@gmail.com>
2023-09-18 10:27:52 +0300

    application descriptions-

--- app/application.py
+++ app/application.py
@@ -2,6 +2,17 @@ from fastapi import FastAPI
 from.api.api_router import api_router
 
 
-app = FastAPI()
+app = FastAPI(
+    title="CurrencyChecker",
+    version="0.0.1",
+    contact={
+        "name": "Roman Kozlov",
+        "github": "https://github.com/SoulRiaper",
+        "email": "soulriaper@gmail.com"
+    },
+    description="""
+        Try this url http://kozlovrs.ru/api/rates?from=USD&to=RUB&amount=1
+    """
+)
 app.include_router(router=api_router, prefix="/api")
 
